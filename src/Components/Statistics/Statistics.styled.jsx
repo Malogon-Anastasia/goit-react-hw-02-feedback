d07cb00f12ed5adb1a180c863a9746b471fd4f0e
Malogon Anastasua <malogonanastasia@gmail.com>
2022-01-13 12:53:54 +0200

    Update Statistics.styled.jsx

--- src/Components/Statistics/Statistics.styled.jsx
+++ src/Components/Statistics/Statistics.styled.jsx
@@ -5,16 +5,10 @@ margin: auto;
 list-style: none;
 padding-left: 10px;
 width: 300px;
-box-shadow: 5px 5px 15px 5px #5c5858;
-  border-radius: 10px;
-  font-family: 'Montserrat', sans-serif;
-  font-weight: 700;
+font-family: 'Montserrat', sans-serif;
+font-weight: 700;
 color: #3e72f7bf;
 letter-spacing: 2px;
-
-&:hover {
-  background-position: right center;
-  }
 `
 
 export const StatisticsItem = styled.li`
