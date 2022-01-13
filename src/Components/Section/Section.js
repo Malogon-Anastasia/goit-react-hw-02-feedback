d410ac0a03bd0b40ae54fb54725256414ceed8a7
Malogon Anastasua <malogonanastasia@gmail.com>
2022-01-13 09:36:51 +0200

    Update Section.js

--- src/Components/Section/Section.js
+++ src/Components/Section/Section.js
@@ -2,8 +2,8 @@ import PropTypes from "prop-types";
 // import './Section.scss';
 
 const Section = ({ title, children }) => (
-  <section className="section">
-    <h2 className="section__name">{title}</h2>
+  <section>
+    <h2>{title}</h2>
     {children}
   </section>
 );
